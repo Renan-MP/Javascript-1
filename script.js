abc135e66b5c3253e66076c8bc3742dad976f0c5
Renan <renanmp1991@outlook.com>
2022-06-17 09:25:55 +0200

    modification image

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function carregar(){
    var msg = window.document.getElementById('msg')
    var img = window.document.getElementById('imagem')
    var data = new Date()
-   var hora = data.getHours()
+   //var hora = data.getHours()
    var minuto = data.getMinutes()
-
+   var hora = 22
    msg.innerHTML = `Agora são ${hora}:${minuto} horas.`
    if (hora >= 0 && hora < 12) {
     //Bom dia!
